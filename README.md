ca989b0a851e20842b49010c885bb25e85f94078
wojciech-baraniak05 <wbaranniakk@gmail.com>
2026-05-17 19:20:35 +0200

    God's beautiful creation

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # proxima_common
-Packages common to both physical and simulated Proxima robot 
+
+
+How to see simulation ( for now rviz2 only - gazebo needs inertia )
+
+```
+colcon build --packages-select proxima_description
+source install/setup.bash # or .zsh
+```
+with that you can run proxima with an arm:
+```
+ros2 launch proxima_description display.launch.py manipulator:=6DOF
+```
+or without:
+```
+ros2 launch proxima_description display.launch.py
+```
+Now that rviz is running, change `Fixed Frame` to `base_link` in the top right corner:
+![Fixed Frame location in rviz](readme_graphics/fixed_frame.png)
+next click `Add` from the bottom left corner, select `RobotModel` and click `OK`:
+![Fixed Frame location in rviz](readme_graphics/adding_model.png)
+In the `Displays` a new `RobotModel` section shoud appear, expand it and change `Description Topic` to `/robot_description`
+![Fixed Frame location in rviz](readme_graphics/desc_topic.png)
+After you click somewhere else on the screen, rvis will update the visualization and display proxima. you can change joint states with `Joint State Publisher` that appeared in another window. Enjoy!!
+![Proxima with random position](readme_graphics/proxima_random.png)
+
+List of all joints in proxima:
+- rod_front_right_upper_joint
+- fork_front_right_joint
+- wheel_front_right_joint
+- rod_front_left_upper_joint
+- fork_front_left_joint
+- wheel_front_left_joint
+- rod_rear_right_upper_joint
+- fork_rear_right_joint
+- wheel_rear_right_joint
+- rod_rear_left_upper_joint
+- fork_rear_left_joint
+- wheel_rear_left_joint
+
+
+List of all joint to controll arm:
+- joint_1
+- joint_2
+- joint_3
+- joint_4
+- joint_5
+- joint_6
--- proxima_description/launch/display.launch.py
+++ proxima_description/launch/display.launch.py
@@ -8,10 +8,10 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
     package_name = 'proxima_description'
-    
     xacro_file_name = 'proxima.urdf.xacro' 
     
     pkg_path = get_package_share_directory(package_name)
+    # Make sure this matches where your file actually is (urdf vs robots)
     xacro_path = os.path.join(pkg_path, 'robots', xacro_file_name)
 
     manipulator_version_arg = DeclareLaunchArgument(
@@ -21,22 +21,35 @@ def generate_launch_description():
     )
 
     robot_description = ParameterValue(
-        Command([
-            'xacro ', xacro_path,
-            ' manipulator:=', LaunchConfiguration('manipulator')
-        ]),
+        Command(['xacro ', xacro_path, ' manipulator:=', LaunchConfiguration('manipulator')]),
         value_type=str
     )
 
+    # 1. The Core Publisher (You already had this)
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
         parameters=[{'robot_description': robot_description}]
+    )
+
+    # 2. The Slider GUI for moving joints
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui'
+    )
+
+    # 3. RViz2 for 3D visualization
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen'
     )
 
     return LaunchDescription([
         manipulator_version_arg,
-        robot_state_publisher_node
+        robot_state_publisher_node,
+        joint_state_publisher_gui_node,
+        rviz_node
     ])
